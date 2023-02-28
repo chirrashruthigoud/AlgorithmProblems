06987a3cc0cf4104a5b66f3a7ae089e5581791e3
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 22:09:10 +0530

    Palindrome Checker

--- Program.cs
+++ Program.cs
@@ -8,54 +8,27 @@ namespace AlgorithmProblems
 {
     public class Program
     {
-        private static HashSet<string> dictionary = new HashSet<string>();
-
-
-        // Returns true if the word can be segmented into parts such
-        // that each part is contained in dictionary
-        public static bool wordBreak(string word)
+        public static void Main(string[] args)
         {
-            int size = word.Length;
 
-            // Base case
-            if (size == 0)
-                return true;
-
-            // Else check for all words
-            for (int i = 1; i <= size; i++)
+            string string1, rev;
+            Console.WriteLine("Enter the String to check is Palindrome or not ");
+            string1 = Console.ReadLine();
+            string s1 = string1.ToLower();
+            char[] ch = s1.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            bool b = s1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
             {
-                // Divide the word into two parts, the prefix will have a length of i
-                // and check if it is present in dictionary, if yes then we will check
-                // for suffix of length size-i recursively. If both prefix and
-                // suffix are present the word is found in dictionary.
-                if (dictionary.Contains(word.Substring(0, i)) &&
-                    wordBreak(word.Substring(i, size - i)))
-                    return true;
+                Console.WriteLine("" + string1 + " is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine(" " + string1 + " is not a Palindrome!");
             }
 
-            // If all cases failed then return false
-            return false;
-        }
-        static void Main(string[] args)
-        {
-            // Array of strings to be added in dictionary HashSet
-            string[] temp_dictionary = { "mobile", "samsung", "sam", "sung",
-                            "man", "mango", "icecream", "and",
-                            "go", "i", "like", "ice", "cream" };
 
-            // Loop to add all strings in dictionary HashSet
-            foreach (string temp in temp_dictionary)
-            {
-                dictionary.Add(temp);
-            }
-
-            // Sample input cases
-            Console.WriteLine(wordBreak("ilikesamsung"));
-            Console.WriteLine(wordBreak("iiiiiiii"));
-            Console.WriteLine(wordBreak(""));
-            Console.WriteLine(wordBreak("ilikelikeimangoiii"));
-            Console.WriteLine(wordBreak("samsungandmango"));
-            Console.WriteLine(wordBreak("samsungandmangok"));
         }
     }
 }

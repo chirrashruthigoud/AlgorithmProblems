7ff34d83d9fc84fc2a0690c15deee07b5dcebaed
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 21:51:25 +0530

    True or False to Show Arithmetic Expression is balanced or not

--- Program.cs
+++ Program.cs
@@ -7,48 +7,125 @@ namespace AlgorithmProblems
 {
     public class Program
     {
-        public static void DisplayFname()
+        public class stack
         {
-            string inputString = "<<name>> First Name ";
-            string firstName = "John"; // Replace with your first name
+            public int top = -1;
+            public char[] items = new char[100];
 
-            string outputString = Regex.Replace(inputString, "<<name>>", firstName);
+            public void push(char x)
+            {
+                if (top == 99)
+                {
+                    Console.WriteLine("Stack full");
+                }
+                else
+                {
+                    items[++top] = x;
+                }
+            }
 
-            Console.WriteLine(outputString);
+            char pop()
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("Underflow error");
+                    return '\0';
+                }
+                else
+                {
+                    char element = items[top];
+                    top--;
+                    return element;
+                }
+            }
+
+            Boolean isEmpty()
+            {
+                return (top == -1) ? true : false;
+            }
         }
-        public static void DisplayFullName()
+
+        // Returns true if character1 and character2
+        // are matching left and right brackets */
+        static Boolean isMatchingPair(char character1,
+                                      char character2)
         {
-            string input = "Hello <<full name>>! How are you?";
-            string fullName = "John Doe"; // Replace this with the actual full name
-
-            string output = Regex.Replace(input, "<<full name>>", fullName);
-            Console.WriteLine(output); // Outputs: Hello John Doe! How are you?
-
+            if (character1 == '(' && character2 == ')')
+                return true;
+            else if (character1 == '{' && character2 == '}')
+                return true;
+            else if (character1 == '[' && character2 == ']')
+                return true;
+            else
+                return false;
         }
 
-        public static void DisplayMobile()
+        // Return true if expression has balanced
+        // Brackets
+        static Boolean areBracketsBalanced(char[] exp)
         {
-            string input = "My mobile number is 91-9876543210. ";
-            string contactNumber = "1234567890";
+            // Declare an empty character stack */
+            Stack<char> st = new Stack<char>();
 
-            string output = Regex.Replace(input, @"91-\d{10}", contactNumber);
-            Console.WriteLine(output);
+            // Traverse the given expression to
+            //   check matching brackets
+            for (int i = 0; i < exp.Length; i++)
+            {
+                // If the exp[i] is a starting
+                // bracket then push it
+                if (exp[i] == '{' || exp[i] == '('
+                    || exp[i] == '[')
+                    st.Push(exp[i]);
+
+                //  If exp[i] is an ending bracket
+                //  then pop from stack and check if the
+                //   popped bracket is a matching pair
+                if (exp[i] == '}' || exp[i] == ')'
+                    || exp[i] == ']')
+                {
+
+                    // If we see an ending bracket without
+                    //   a pair then return false
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    // Pop the top element from stack, if
+                    // it is not a pair brackets of
+                    // character then there is a mismatch. This
+                    // happens for expressions like {(})
+                    else if (!isMatchingPair(st.Pop(),
+                                             exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // If there is something left in expression
+            // then there is a starting bracket without
+            // a closing bracket
+
+            if (st.Count == 0)
+                return true; // balanced
+            else
+            {
+                // not balanced
+                return false;
+            }
         }
-        public static void DisplayCurrentDate()
-        {
-            string input = "Today's date is 12/25/2063";
-            DateTime currentDate = DateTime.Now.Date;
 
-            string output = Regex.Replace(input, @"\d{2}/\d{2}/\d{4}", currentDate.ToString("MM/dd/yyyy"));
-            Console.WriteLine(output); // Outputs: Today's date is 02/28/2023. Please confirm the order by 02/28/2023.
-        }
-
-        static void Main(string[] args)
+        // Driver code
+        public static void Main(String[] args)
         {
-            Program.DisplayFname();
-            Program.DisplayFullName();
-            Program.DisplayMobile();
-            Program.DisplayCurrentDate();
+            char[] exp = { '{', '(', ')', '}', '[', ']' };
+
+            // Function call
+            if (areBracketsBalanced(exp))
+                Console.WriteLine("Balanced ");
+            else
+                Console.WriteLine("Not Balanced ");
         }
 
     }

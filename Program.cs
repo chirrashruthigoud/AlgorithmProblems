c411664865b6a31b9ed026f4952c45780556bc7c
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 12:29:38 +0530

    Algorithm Programs

--- Program.cs
+++ Program.cs
@@ -2,45 +2,9 @@
 {
     public class Program
     {
-        public static void swap(char[] chars, int i, int j)
-        {
-            char temp = chars[i];
-            chars[i] = chars[j];
-            chars[j] = temp;
-        }
-
-        public static void permutations(char[] chars, int currentIndex)
-        {
-            if (currentIndex == chars.Length - 1)
-            {
-                Console.WriteLine(chars);
-            }
-
-            for (int i = currentIndex; i < chars.Length; i++)
-            {
-                swap(chars, currentIndex, i);
-                permutations(chars, currentIndex + 1);
-                swap(chars, currentIndex, i);
-            }
-        }
-
-        public static void findPermutations(String str)
-        {
-
-            // base case
-            if (str == null || str.Length == 0)
-            {
-                return;
-            }
-
-            permutations(str.ToCharArray(), 0);
-        }
         public static void Main(string[] args)
         {
-            String str = "ABC";
-            findPermutations(str);
-
-
-        }
+            Console.WriteLine("Welcome to Algorithm Programs ");
+                }
     }
 }
